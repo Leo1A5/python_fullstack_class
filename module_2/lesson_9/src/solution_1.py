b5e34fd77b05fa8933d9f14e12410c0e350bcda3
Nikolay Martihin <martixin000@mail.ru>
2023-12-25 01:55:14 +0300

    third

--- module_2/lesson_9/src/solution_1.py
+++ module_2/lesson_9/src/solution_1.py
@@ -1,10 +1,7 @@
-price_list = [float(x) for x in input("Введите цены и скидку: ").split()]
 def calculate_discount(prices):
     price_list = prices[:-1]
     discount = sum(price_list) * (prices[-1] / 100)
     return discount
 
-total_discount = calculate_discount(price_list)
-
-print("Сумма скидки: ", total_discount)
-
+print(f"Сумма скидки: {calculate_discount([100, 200, 300, 10])}")
+print(f"Сумма скидки: {calculate_discount([50, 150, 250, 20])}")
--- module_2/lesson_9/src/solution_2.py
+++ module_2/lesson_9/src/solution_2.py
@@ -1,9 +1,10 @@
-rgb = [int(x) for x in input("RGB: ").split()]
 def convert_to_hex(rgb):
     if len(rgb) != 3:
         print("Ошибка")
     hex_color = f"#{rgb[0]:02X}{rgb[1]:02X}{rgb[2]:02X}"
     return hex_color
 
-hex_format = convert_to_hex(rgb)
-print(hex_format)
+
+print(f"HEX: {convert_to_hex([255, 0, 0])}")
+print(f"HEX: {convert_to_hex([0, 255, 0])}")
+print(f"HEX: {convert_to_hex([0, 0, 255])}")
