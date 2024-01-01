cc2cf6e7ff6e4a1b23eeabfe2a799258b56480ff
Nikolay Martihin <martixin000@mail.ru>
2024-01-01 13:55:55 +0300

    second

--- module_2/lesson_10/src/solution_6.py
+++ module_2/lesson_10/src/solution_6.py
@@ -1,1 +1,16 @@
-def find_max_price(price):
+price_list = [10, 8, 12]
+def find_max_price(price_list):
+    if len(price_list) == 0:
+        return None
+    elif len(price_list) == 1:
+        return price_list[0]
+    else:
+        max_price = find_max_price(price_list[1:])
+        return price_list[0] if price_list[0] > max_price else max_price
+
+result = find_max_price(price_list)
+print(result)
+"""15, 7, 9
+1, 10, 20, 5
+25, 25, 25
+10, 8, 12"""
--- module_2/lesson_10/src/solution_7.py
+++ module_2/lesson_10/src/solution_7.py
@@ -1,9 +1,17 @@
-def calculate_discount(*early, **now,):
-    finaly_price :float = now - (early * 0.1)
-    return finaly_price
+prices = (100, 200, 300, 400)
+def calculate_discount(prices, index=0, previous_price=None):
+    end_prices = []
 
+    if index < len(prices):
+        current_price = prices[index] 
+        current_discount = previous_price * 0.1 if previous_price is not None else 0
+        discount_price = current_price - current_discount if previous_price is not None else current_price
+        end_prices.append(discount_price)
+        end_prices += calculate_discount(prices, index + 1, current_price)
 
-print(calculate_discount(1000, 2000, 3000))
-print(calculate_discount(5000, 10000, 15000))
-print(calculate_discount(100, 200, 300, 400))
-print(calculate_discount(50, 100))
+    return end_prices
+print(calculate_discount(prices))
+"""1000, 2000, 3000
+5000, 10000, 15000
+100, 200, 300, 400
+50, 100"""
