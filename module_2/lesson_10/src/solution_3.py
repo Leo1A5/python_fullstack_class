d81e3197d6b590031e3808b5c0a049791c456b9a
Nikolay Martihin <martixin000@mail.ru>
2024-01-02 11:23:51 +0300

    third

--- module_2/lesson_10/src/solution_3.py
+++ module_2/lesson_10/src/solution_3.py
@@ -1,11 +1,12 @@
 workers_list :dict = {"Джон" : 1, "Майк" : 1, "Эмили" : 1}
+
 def workers(workers_list):
     names_worker = list(workers_list.keys())
     values_worker= list(workers_list.values())
     max_kpi = max(values_worker)
-    responsible_worker :dict= [name for name, completed in workers_list.items() if completed == max_kpi]
-
-    return responsible_worker
+    responsible_worker = [name for name, completed in workers_list.items() if completed == max_kpi]
+    result = ", ".join(responsible_worker)
+    return result
 
 responsible_worker = workers(workers_list)
 
--- module_2/lesson_10/src/solution_7.py
+++ module_2/lesson_10/src/solution_7.py
@@ -1,16 +1,18 @@
 prices = (100, 200, 300, 400)
 def calculate_discount(prices, index=0, previous_price=None):
     end_prices = []
-
+    
     if index < len(prices):
         current_price = prices[index] 
         current_discount = previous_price * 0.1 if previous_price is not None else 0
         discount_price = current_price - current_discount if previous_price is not None else current_price
         end_prices.append(discount_price)
         end_prices += calculate_discount(prices, index + 1, current_price)
-
+                
     return end_prices
-print(calculate_discount(prices))
+finish_price = calculate_discount(prices)
+finish_price = [int (x) for x in finish_price]
+print(*finish_price, sep = ", ")
 """1000, 2000, 3000
 5000, 10000, 15000
 100, 200, 300, 400
