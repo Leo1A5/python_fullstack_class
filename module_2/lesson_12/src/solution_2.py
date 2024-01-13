5907ac3bfda194c1fbb1a22375b3beabe370906d
Nikolay Martihin <martixin000@mail.ru>
2024-01-13 14:16:06 +0300

    four

--- module_2/lesson_12/src/solution_2.py
+++ module_2/lesson_12/src/solution_2.py
@@ -1,15 +1,20 @@
 order1 = {"id": 1, "items": ["book", "pen"]}
 order2 = {"id": 2, "items": []}
-
 def check_order(order):
-    return bool(order.get("items"))
+    global check
+    check = bool(order.get("items"))
+    return check
 def package_order(check, order):
-    if check(order) == True:
+    if check:
         package = f"Упакован заказ: {order['id']}"
         return package
+    else:
+        return f"Заказ {order['id']}: пуст"
 def send_order(check, package, order):
-    if check == True:
+    if check:
         return f"Отправка: {package}"
+    else: 
+        return f"Отправка: {package}"   
 
-print(send_order(check_order, package_order, order1))
-print(send_order(check_order, package_order, order2))
+print(send_order(check_order(order1), package_order(check, order1), order1))
+print(send_order(check_order(order2), package_order(check, order2), order2))
--- module_2/lesson_13/src/solution_1.py
+++ module_2/lesson_13/src/solution_1.py
@@ -1,12 +1,22 @@
 order1 = {"id": 1, "items": ["book", "pen"]}
 order2 = {"id": 2, "items": []}
-
 def check_order(order):
-    return bool(order.get("items"))
+    global check
+    check = bool(order.get("items"))
+    return check
 def package_order(check, order):
-    if check(order) == True:
-        return f"Упакован заказ {order['id']}"
+    if check:
+        package = f"Упакован заказ: {order['id']}"
+        return package
+    else:
+        return f"Заказ {order['id']}: пуст"
 def send_order(check, package, order):
-    return f"Отправка: {package}"
+    if check:
+        return f"Отправка: {package}"
+    else: 
+        return f"Отправка: {package}"    
+    
 
-print(check_order(package_order(), order)
+print(send_order(check_order(order1), package_order(check, order1), order1))
+print(send_order(check_order(order2), package_order(check, order2), order2))
+
