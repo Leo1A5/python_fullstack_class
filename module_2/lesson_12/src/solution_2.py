e646adf5dbce3765a3d41b35d9b5284b7a07c938
Nikolay Martihin <martixin000@mail.ru>
2024-01-16 00:27:59 +0300

    xm

--- module_2/lesson_12/src/solution_2.py
+++ module_2/lesson_12/src/solution_2.py
@@ -3,12 +3,12 @@ order2 = {"id": 2, "items": []}
 def check_order(order):
     return bool(order.get("items"))
 def package_order(order):
-    return bool(order)
+    return f"Упакован заказ {order['id']}"
 def send_order(check, package, order):
     if check(order):
-        return f"Отправка: Упакован заказ {order['id']}" if package(order) else f"Нечего упаковывать"
+        return f"Отправка: {order['id']}" # в этом случае работает
     else:
-        return f"Отправка: заказ {order['id']} пуст"
+        return f"Отправка: {package} пуст" # в этом нет
 
 print(send_order(check_order, package_order, order1))
 print(send_order(check_order, package_order, order2))
