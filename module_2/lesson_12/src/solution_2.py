fc670d82a60854065777924d74d8391a57c3edf0
Nikolay Martihin <martixin000@mail.ru>
2024-01-15 14:46:26 +0300

    five

--- module_2/lesson_12/src/solution_2.py
+++ module_2/lesson_12/src/solution_2.py
@@ -1,18 +1,14 @@
 order1 = {"id": 1, "items": ["book", "pen"]}
 order2 = {"id": 2, "items": []}
 def check_order(order):
-    global check
-    check = bool(order.get("items"))
-    return check
-def package_order(check, order):
-    if check:
-        package = f"Упакован заказ: {order['id']}"
-        return package
-    else:
-        return f"Заказ {order['id']}: пуст"
+    return bool(order.get("items"))
+def package_order(order):
+    return bool(order)
 def send_order(check, package, order):
-    return f"Отправка: {package}"
+    if check(order):
+        return f"Отправка: Упакован заказ {order['id']}" if package(order) else f"Нечего упаковывать"
+    else:
+        return f"Отправка: заказ {order['id']} пуст"
 
-
-print(send_order(check_order(order1), package_order(check, order1), order1))
-print(send_order(check_order(order2), package_order(check, order2), order2))
+print(send_order(check_order, package_order, order1))
+print(send_order(check_order, package_order, order2))
