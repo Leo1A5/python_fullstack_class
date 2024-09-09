2102c1cfc7aa59ece1e25ea1388283bb862bf0d8
Nikolay Martihin <martixin000@mail.ru>
2024-09-09 14:20:57 +0300

    second

--- module_2/lesson_15/src/solution_1.py
+++ module_2/lesson_15/src/solution_1.py
@@ -1,25 +1,20 @@
-def add_Client(name: str, history: dict):
-
-    client_id = len(history)+1
-
+def add_client(name, history):
+    client_id = len(history) + 1
     history[client_id] = {'name': name, 'orders': []}
 
 
-def Make_order(client_id: int, history: dict, order_details: dict):
-
+def make_order(client_id, history, order_details):
     history[client_id]['orders'].append(order_details)
 
 
-def GET_HISTORY(client_id: int, history: dict):
-
+def get_history(client_id, history):
     return history[client_id]['orders']
 
 
 client_history = {}
 
+add_client('Roman', client_history)
+make_order(1, client_history, {'order_id': 1, 'amount': 100})
 
-add_Client('Roman', client_history)
 
-Make_order(1, client_history, {'order_id': 1, 'amount': 100})
-
-print(GET_HISTORY(1, client_history))
+print(get_history(1, client_history))
